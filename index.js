5a8f1795a9e8576adc8d9467cd2bb892d68c2bff
Drew Gallagher <drewg2009@gmail.com>
2025-02-09 10:57:08 -0400

    fix global pattern to find files

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const options = {
   // Base directory which we use to locate your JSDOC files
   baseDir: __dirname,
   // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
-  filesPattern: './**/*.js',
+  filesPattern: '*.js',
   // URL where SwaggerUI will be rendered
   swaggerUIPath: '/api-docs',
   // Expose OpenAPI UI
