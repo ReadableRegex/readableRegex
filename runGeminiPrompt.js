62ad306bd63de24a450ca05fd381327583cfad6c
Drew Gallagher <drewg2009@gmail.com>
2025-02-16 01:21:19 -0400

    doc

--- runGeminiPrompt.js
+++ runGeminiPrompt.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: ".env" });
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 async function fetchAiGeneratedContent(prompt) {
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); //setup gemini model
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); // setup gemini model
     const result = await model.generateContent([prompt]); // run the model with the prompt
     return result.response.text(); // return the string result
   } catch (e) {
